12bb1925e88694b273cb3997869c6e3d7d7ce2a3
omegaatt36 <omegaatt36@gmail.com>
2022-07-12 20:33:03 +0800

    add promethues exporter.

--- raspberrypi/main.go
+++ raspberrypi/main.go
@@ -1,19 +1,84 @@
 package main
 
 import (
-	"context"
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
+	"net/http"
 	"time"
 
 	"github.com/d2r2/go-bsbmp"
 	"github.com/d2r2/go-i2c"
 	"github.com/d2r2/go-logger"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+type bmp280Collector struct {
+	temperatureMetric *prometheus.Desc
+	pressureMetric    *prometheus.Desc
+	altitudeMetric    *prometheus.Desc
+
+	sensor *bsbmp.BMP
+}
+
+func newBmp280Collector(bmp280 *bsbmp.BMP) *bmp280Collector {
+	return &bmp280Collector{
+		temperatureMetric: prometheus.NewDesc(
+			"bmp280_temperature", "bmp 280 temperature(Celsius)",
+			nil, nil),
+		pressureMetric: prometheus.NewDesc(
+			"bmp280_pressure", "bmp 280 pressure(hPa)",
+			nil, nil),
+		altitudeMetric: prometheus.NewDesc(
+			"bmp280_altitude", "bmp 280 altitude(m)",
+			nil, nil),
+
+		sensor: bmp280,
+	}
+}
+
+//Each and every collector must implement the Describe function.
+//It essentially writes all descriptors to the prometheus desc channel.
+func (collector *bmp280Collector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- collector.temperatureMetric
+	ch <- collector.pressureMetric
+	ch <- collector.altitudeMetric
+}
+
+//Collect implements required collect function for all promehteus collectors
+func (collector *bmp280Collector) Collect(ch chan<- prometheus.Metric) {
+	// Read temperature in celsius degree
+	temperature, err := collector.sensor.ReadTemperatureC(bsbmp.ACCURACY_STANDARD)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	// Read atmospheric pressure in pascal
+	pressure, err := collector.sensor.ReadPressurePa(bsbmp.ACCURACY_STANDARD)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	// Read atmospheric altitude in meters above sea level, if we assume
+	// that pressure at see level is equal to 101325 Pa.
+	altitude, err := collector.sensor.ReadAltitude(bsbmp.ACCURACY_STANDARD)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+
+	mTemperature := prometheus.MustNewConstMetric(collector.temperatureMetric,
+		prometheus.GaugeValue, float64(temperature))
+	mPressure := prometheus.MustNewConstMetric(collector.pressureMetric,
+		prometheus.GaugeValue, float64(pressure))
+	mAltitude := prometheus.MustNewConstMetric(collector.altitudeMetric,
+		prometheus.GaugeValue, float64(altitude))
+	ch <- prometheus.NewMetricWithTimestamp(time.Now(), mTemperature)
+	ch <- prometheus.NewMetricWithTimestamp(time.Now(), mPressure)
+	ch <- prometheus.NewMetricWithTimestamp(time.Now(), mAltitude)
+}
+
 func main() {
+
 	// Create new connection to i2c-bus on 1 line with address 0x76.
 	// Use i2cdetect utility to find device address over the i2c-bus
 	i2c, err := i2c.NewI2C(0x76, 1)
@@ -31,40 +96,11 @@ func main() {
 	// Uncomment next line to supress verbose output
 	logger.ChangePackageLogLevel("bsbmp", logger.InfoLevel)
 
-	ctx := context.Background()
-	ch := make(chan os.Signal, 1)
+	collector := newBmp280Collector(sensor)
+	prometheus.MustRegister(collector)
 
-	go func(ctx context.Context) {
-		for {
-			ctx.Done()
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":9110", nil))
 
-			// Read temperature in celsius degree
-			t, err := sensor.ReadTemperatureC(bsbmp.ACCURACY_STANDARD)
-			if err != nil {
-				log.Fatal(err)
-			}
-			// Read atmospheric pressure in pascal
-			pPa, err := sensor.ReadPressurePa(bsbmp.ACCURACY_STANDARD)
-			if err != nil {
-				log.Fatal(err)
-			}
-			// Read atmospheric altitude in meters above sea level, if we assume
-			// that pressure at see level is equal to 101325 Pa.
-			a, err := sensor.ReadAltitude(bsbmp.ACCURACY_STANDARD)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			log.Printf("Temprature = %vC\tPressure = %v Pa\tAltitude = %v m\n", t, pPa, a)
-
-			time.Sleep(time.Second)
-		}
-	}(ctx)
-
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	<-ch
-	log.Println("terminating...")
-
-	ctx.Done()
 	log.Println("exit")
 }
